aea05066d1da9f6ef2592715ba2e167e55d6d881
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-08-09 23:14:40 +0500

    Add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+var express = require('express')
+var cors = require('cors')
 
 const app = express();
+app.use(cors());
 app.listen(3000, () => {console.log('listening on port 3000')})
