7e82cefc7804e63a18390810e53ff07da2093ac4
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-08-10 16:42:40 +0500

    Add productSchema

---
+++ backend/src/models/product.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+interface IImage {
+  filename: string;
+  originalName: string;
+}
+
+interface IProduct {
+  title: string;
+  image: { fileName: string; originalName: string };
+  category: string;
+  description: string;
+  price: number;
+}
+
+const imageSchema = new mongoose.Schema<IImage>({
+  filename: {
+    type: String,
+    required: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
+});
+
+const productSchema = new mongoose.Schema<IProduct>({
+  title: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  image: imageSchema,
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  price: {
+    type: Number,
+    required: false,
+    default: null,
+  },
+});
+
+export default mongoose.model<IProduct>("product", productSchema);
