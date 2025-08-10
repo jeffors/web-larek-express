d52e5ab1cbf3f1c01258e42ef39c1457df7048a8
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-08-10 15:51:59 +0500

    Add mongoose

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,11 @@
-var express = require('express')
-var cors = require('cors')
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import 'dotenv/config'
 
 const app = express();
+mongoose.connect(process.env.DB_ADDRESS!);
 app.use(cors());
-app.listen(3000, () => {console.log('listening on port 3000')})
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
