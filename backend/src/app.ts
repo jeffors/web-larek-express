0f9b5c7a1ba57f5fa713a986e46c6391a9013f55
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-08-11 17:03:06 +0500

    Add static files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,15 +3,15 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import router from "./routes/product";
+import path from "path";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 mongoose.connect(process.env.DB_ADDRESS!);
 app.use(cors());
 app.use("/product", router);
-
+app.use(express.static(path.join(__dirname, "../public")));
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
