1a9ede7b6638f4a911ca57a670faa25490ec3424
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-08-11 16:31:44 +0500

    Add get and post product routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import 'dotenv/config'
+import "dotenv/config";
+import router from "./routes/product";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.DB_ADDRESS!);
 app.use(cors());
+app.use("/product", router);
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
---
+++ backend/src/controllers/products.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import product from "../models/product";
+
+export const getProducts = async (req: Request, res: Response) => {
+  return product
+    .find({})
+    .then((products) => res.send({ products }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+export const createProduct = async (req: Request, res: Response) => {
+  const { title, image, category, description, price } = req.body;
+  return product
+    .create({ title, image, category, description, price })
+    .then((product) => res.send({ product }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
