cfbd8c002484513eb60e3a63b44eefd38acc8fc3
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-08-16 00:10:29 +0500

    Validate order

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,8 +1,32 @@
 import { faker } from "@faker-js/faker/locale/ru";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import product from "models/product";
 
-export const createOrder = async (req: Request, res: Response) => {
-  const { payment, email, phone, address, total, items } = req.body;
+export const createOrder = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { total, items } = req.body;
+  const products = await product.find({ _id: { $in: items } });
+
+  if (products.length !== items.length) {
+    return res.status(400).send({ message: "Указанные товары не найдены" });
+  }
+  for (const product of products) {
+    if (product.price === null) {
+      return res.status(400).send({ message: "Указанный товар не продаётся" });
+    }
+  }
+  const productsTotal = products.reduce(
+    (sum, product) => sum + product.price,
+    0
+  );
+  if (productsTotal !== total) {
+    return res
+      .status(400)
+      .send({ message: "Сумма заказа не совпадает с суммой цен товаров" });
+  }
   const orderId = faker.string.uuid();
-  res.status(200).send({ id: orderId, total: total });
+  return res.status(200).send({ id: orderId, total: total });
 };
