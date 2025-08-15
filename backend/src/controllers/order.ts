2586d6a1127ea26e78e155cc5353231b85f12fe9
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-08-15 17:52:06 +0500

    Add faker

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,6 +1,8 @@
+import { faker } from "@faker-js/faker/locale/ru";
 import { Request, Response } from "express";
 
 export const createOrder = async (req: Request, res: Response) => {
   const { payment, email, phone, address, total, items } = req.body;
-  res.status(200).send({ id: "fakeid", total: total });
+  const orderId = faker.string.uuid();
+  res.status(200).send({ id: orderId, total: total });
 };
